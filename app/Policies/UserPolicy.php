da4d4c9992bed81042438dba26dbd13d7b9da928
amir.syafiq989@gmail.com <amir.syafiq989@gmail.com>
2022-12-19 14:07:49 +0800

    update UserPolicy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,85 +9,27 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
+    public function update(User $current_user, User $user)
     {
-        //
+        if ($user->hasRole('Admin')) {
+            return false;
+        } else {
+            return $current_user->id != $user->id;
+        }
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, User $model)
+    public function profile(User $current_user, User $user)
     {
-        //
+        return $current_user->id === $user->id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
+    public function password(User $current_user, User $user)
     {
-        //
+        return $current_user->id === $user->id;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, User $model)
+    public function destroy(User $current_user, User $user)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, User $model)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, User $model)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, User $model)
-    {
-        //
+        return $current_user->hasRole('Admin') && $current_user->id !== $user->id;
     }
 }
